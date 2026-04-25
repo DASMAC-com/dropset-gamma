136a92734ed80ac0c496d3344a96d99ff29ec193
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-04-24 17:37:06 -0700

    Add errors

--- program/src/errors.rs
+++ program/src/errors.rs
@@ -2,6 +2,10 @@ use anchor_lang_v2::prelude::*;
 
 #[error_code]
 pub enum DropsetError {
-    #[msg("Init instruction must be called by program ID")]
-    InvalidInitSigner,
+    #[msg("program_data account is not the canonical PDA for this program")]
+    InvalidProgramDataAddress,
+    #[msg("program_data account contents could not be decoded")]
+    InvalidProgramData,
+    #[msg("Init must be signed by the program's upgrade authority")]
+    InvalidUpgradeAuthority,
 }
